0ca49d773d9739f51d52f2221fe2d9100544ffd7
0097eo <emmanuelokello294@gmail.com>
2024-10-29 22:59:21 +0300

    change color if uodate button to gray

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -151,7 +151,7 @@ const AdminDashboard = () => {
               </button>
               <button
                 type="submit"
-                className="px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                className="px-4 py-2 text-sm font-medium text-white bg-gray-600 rounded-md hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
               >
                 Update Status
               </button>
